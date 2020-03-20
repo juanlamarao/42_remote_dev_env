5a3834f93ca94cf8104edc064179ad087980c685
Juan Lamarão <oliveiralamarao@gmail.com>
2020-03-20 15:05:06 -0300

    Update run_container.sh

--- run_container.sh
+++ run_container.sh
@@ -1,9 +1,9 @@
 #!/usr/bun/env bash
 
 # change "42_intra_id", "My Name" and "me@email.com" as may suit you
-INTRA_ID="42_intra_id"
-GIT_NAME="My Name"
-GIT_MAIL="me@mail.com"
+LOCAL_INTRA_ID="42_intra_id"
+LOCAL_GIT_NAME="My Name"
+LOCAL_GIT_EMAIL="me@mail.com"
 
 # ------------------------------------------------------------------- #
 # don't change this part
@@ -11,7 +11,7 @@ docker run -it --rm \
     -v ~/docker_files:/workdir \
     -e HOST_USER_ID=$(id -u $USER) \
     -e HOST_GROUP_ID=$(id -g $USER) \
-    -e 42_INTRA_ID=$INTRA_ID \
-    -e GIT_USER_NAME=$GIT_NAME \
-    -e GIT_USER_EMAIL=$GIT_MAIL \
+    -e 42_INTRA_ID=$LOCAL_INTRA_ID \
+    -e GIT_USER_NAME=$LOCAL_GIT_NAME \
+    -e GIT_USER_EMAIL=$LOCAL_GIT_EMAIL \
     remote_42
