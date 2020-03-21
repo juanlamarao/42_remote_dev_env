535e2f4158fda08e8273811f9557ac849add17a6
Juan Lamarão <oliveiralamarao@gmail.com>
2020-03-21 04:36:54 -0300

    Update run_container.sh

--- run_container.sh
+++ run_container.sh
@@ -4,18 +4,20 @@
 #docker build . -t remote_42_img
 
 # change "42_intra_id", "My Name" and "me@email.com" as may suit you
-LOCAL_INTRA_ID="42_intra_id"
-LOCAL_GIT_NAME="My Name"
-LOCAL_GIT_EMAIL="me@mail.com"
+L_INTRA_ID="42_intra_id"
+L_GIT_NAME="My Name"
+L_GIT_EMAIL="me@mail.com"
 
 # ------------------------------------------------------------------- #
 # don't change this part
+L_USER=$(whoami)
+
 docker run -it --rm \
 	--name=remote_42_ctn \
 	-v ~/docker_files:/docker_files \
-	-e HOST_USER_ID=$(id -u $USER) \
-	-e HOST_GROUP_ID=$(id -g $USER) \
-	-e INTRA_ID="$(echo "$LOCAL_INTRA_ID")" \
-	-e GIT_USER_NAME="$(echo "$LOCAL_GIT_NAME")" \
-	-e GIT_USER_EMAIL="$(echo "$LOCAL_GIT_EMAIL")" \
+	-e HOST_USER_ID=$(id -u "$L_USER") \
+	-e HOST_GROUP_ID=$(id -g "$L_USER") \
+	-e INTRA_ID="$(echo "$L_INTRA_ID")" \
+	-e GIT_USER_NAME="$(echo "$L_GIT_NAME")" \
+	-e GIT_USER_EMAIL="$(echo "$L_GIT_EMAIL")" \
     remote_42_img
